807e1189919c0aac40cf5c7f39e045f2b3f569ad
박성원 <sw2park@gridone.co.kr>
2023-05-02 18:34:50 +0900

    7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 import Button from "./Button";
 
 import styles from "./App.module.css"
 import { useState, useEffect } from "react";
+import userEvent from "@testing-library/user-event";
 
+import Movie from "./components/Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 
 function App() {
@@ -108,41 +117,76 @@ function App() {
 //   )
 ///////////////////////////////////////todo List .end  
 
-const [loading, setLoading] = useState(true);
-const [coin, setCoin] = useState([]);
+
+//////////////////////////////////////coin tracker .start
+// const [loading, setLoading] = useState(true);
+// const [coin, setCoin] = useState([]);
 
 
-useEffect(()=>{
-  fetch("https://api.coinpaprika.com/v1/tickers").then((res)=> res.json())
-  .then((json)=> {
-    setCoin(json);
-    setLoading(false);
-  });
-}, []);
+// useEffect(()=>{
+//   fetch("https://api.coinpaprika.com/v1/tickers").then((res)=> res.json())
+//   .then((json)=> {
+//     setCoin(json);
+//     setLoading(false);
+//   });
+// }, []);
 
-console.log(coin);
+// console.log(coin);
+
+// return (
+//   <div>
+//     <h1>The Coins !!! ({coin.length})</h1>
+//     {loading? <strong>loading...</strong>:null}
+
+//     <ul>
+//       {coin.map((coin, index)=>
+//       <li key={index}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
+//       )}
+//     </ul>
+
+//     <select>
+    
+//       {coin.map((coin, index)=>
+//       <option key={index}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD</option>
+//       )}
+    
+//     </select>
+//   </div>
+// )
+//////////////////////////////////////coin tracker .end
+
+//https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
+
 
 return (
-  <div>
-    <h1>The Coins !!! ({coin.length})</h1>
-    {loading? <strong>loading...</strong>:null}
-
-    <ul>
-      {coin.map((coin, index)=>
-      <li key={index}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
-      )}
-    </ul>
-
-    <select>
-    
-      {coin.map((coin, index)=>
-      <option key={index}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD</option>
-      )}
-    
-    </select>
-  </div>
-)
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Detail />} />
+    </Routes>
+  </Router>
+);
 
 }
 
 export default App;
+
+
+
+
+
+
+
+// https://nodejs.org/en
+// npx create-react-app ~~~~(프로젝트명)
+
+
+// -> terminal
+// npm run
+// npm start
+
+
+// propTypes install
+// -> npm i prop-types
+
+// npm install react-router-dom
---
+++ src/components/Movie.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
+function Movie({ title, summary, genres, img }) {
+    return (
+        <div>
+            <h2>
+            <Link to="/movie">{title}</Link>
+            </h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((e, i) => (
+                    <li key={i}>{e}</li>
+                ))}
+            </ul>
+            <img src={img} alt={title}/>
+            <hr />
+        </div>
+    );
+}
+
+
+Movie.propTypes={
+    title:PropTypes.string.isRequired,
+    summary:PropTypes.string.isRequired,
+    img:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired
+}
+export default Movie;
